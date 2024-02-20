2ea226cdb952ed276cf0ec9dbb8bc1b56d8bdd1c
poyrakie <johan.astrom91@live.se>
2024-02-20 16:35:41 +0100

    Add project files.

---
+++ Silicon/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Welcome";
+        return View();
+    }
+}

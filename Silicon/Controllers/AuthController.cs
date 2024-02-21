ce45a37585d30061730cc9b88f8a97662040d4a6
poyrakie <johan.astrom91@live.se>
2024-02-21 15:04:48 +0100

    Signin done, some functionality and stuff still needs to be implemented (api communication etc)

--- Silicon/Controllers/AuthController.cs
+++ Silicon/Controllers/AuthController.cs
@@ -24,4 +24,30 @@ public class AuthController : Controller
         }
         return RedirectToAction("SignIn", "Auth");
     }
+
+    [Route("/signin")]
+    [HttpGet]
+    public IActionResult SignIn()
+    {
+        var viewModel = new SignInViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/signin")]
+    [HttpPost]
+    public IActionResult SignIn(SignInViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+
+            return View(model);
+        }
+
+        //var result = _authService.SignIn(model.Form);
+        //if (result)
+        //{ return RedirectToAction("Account", "Index"); }
+
+        model.ErrorMessage = "Incorrect email or password";
+        return View(model);
+    }
 }

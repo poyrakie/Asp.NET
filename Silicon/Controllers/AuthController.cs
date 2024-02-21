b91e4d3b0e00d9eaace915ac94870223b126a3ab
poyrakie <johan.astrom91@live.se>
2024-02-21 13:54:54 +0100

    Signup complete. Must still get connected to other stuff (api etc)

--- Silicon/Controllers/AuthController.cs
+++ Silicon/Controllers/AuthController.cs
@@ -1,13 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Silicon.ViewModels;
 
 namespace Silicon.Controllers;
 
 public class AuthController : Controller
 {
     [Route("/signup")]
+    [HttpGet]
     public IActionResult SignUp()
     {
-        ViewData["Title"] = "Sign up";
-        return View();
+        var viewModel = new SignUpViewModel();
+        return View(viewModel);
+    }
+
+    [Route("/signup")]
+    [HttpPost]
+    public IActionResult SignUp(SignUpViewModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            model.ErrorMessage = "Password must be one uppercase, one lowercase, one number, and one special character.And a minimum of 8 characters";
+            return View(model);
+        }
+        return RedirectToAction("SignIn", "Auth");
     }
 }

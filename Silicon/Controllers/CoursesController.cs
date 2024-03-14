80d2ca29ef2def4d5c4ad3b27bf0d1b6aa5fb88b
poyrakie <johan.astrom91@live.se>
2024-03-14 13:17:30 +0100

    Courses init

---
+++ Silicon/Controllers/CoursesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+
+[Authorize]
+public class CoursesController : Controller
+{
+    [HttpGet]
+    [Route("/courses")]
+    public IActionResult Index()
+    {
+        ViewData["Title"] = "Courses";
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult SingleCourse()
+    {
+        return View();
+    }
+    [HttpPost]
+    public IActionResult Bookmark()
+    {
+        return View();
+    }
+}

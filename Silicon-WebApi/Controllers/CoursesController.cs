4543f55b152405290c35b6ad8584211ce4646ec9
poyrakie <johan.astrom91@live.se>
2024-03-21 11:56:35 +0100

    Web api init

---
+++ Silicon-WebApi/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Silicon_WebApi.Models;
+
+namespace Silicon_WebApi.Controllers;
+
+[Route("api/courses")]
+[ApiController]
+public class CoursesController : ControllerBase
+{
+    private List<CoursesModel> _courses = [];
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+
+        return Ok();
+    }
+
+    [HttpPost]
+    public IActionResult Create(CoursesModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            return Created("", model);
+        }
+        return BadRequest();
+    }
+}
---
+++ Silicon-WebApi/Models/CoursesModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Silicon_WebApi.Models;
+
+public class CoursesModel
+{
+    public string Id { get; set; } = null!;
+    public string Title { get; set; } = null!;
+    public string? ImageName { get; set; }
+    public string? Author { get; set; }
+    public bool IsBestSeller { get; set; }
+    public int Hours { get; set; }
+    public decimal OriginalPrice { get; set; }
+    public decimal DiscountPrice { get; set; }
+    public decimal LikesInProcent { get; set; }
+    public decimal LikesInNumbers { get; set; }
+}

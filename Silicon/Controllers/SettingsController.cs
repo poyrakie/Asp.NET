2545afac21ff1387e59744457333d0323d3a3067
poyrakie <johan.astrom91@live.se>
2024-04-01 12:28:46 +0200

    Dark mode up and running. Fully functional

---
+++ Silicon/Controllers/SettingsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Silicon.Controllers;
+
+public class SettingsController : Controller
+{
+    public IActionResult ChangeTheme(string theme)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(365),
+        };
+        Response.Cookies.Append("ThemeMode", theme, option);
+        return Ok();
+    }
+}
---
+++ Silicon/wwwroot/js/slider.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let sw = document.querySelector('#switch-mode')
+
+    sw.addEventListener('change', function () {
+        let theme = this.checked ? "dark" : "light"
+
+        fetch(`/Settings/changetheme?theme=${theme}`)
+            .then(res => {
+                if (res.ok)
+                    window.location.reload()
+                else {
+                    console.log('Something went wrong with darkmode')
+                }
+            })
+    })
+})
